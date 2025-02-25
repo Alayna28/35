9e4182ba0b91303304b018e486426c573e973bd5
Alayna28 <alaynabaloch100@gmail.com>
2025-02-25 20:34:59 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+#printing words
+print("Hello World!")
+#printing numbers
+print(5)
+#printing a comination of words and number
+print("Hii!", 5)
+#printing the end statement
+print("Welcome to", end="*")
---
+++ main2.py
@@ -0,0 +1,12 @@
+# creating variables
+x = 5
+y = "Alayna"
+print(x)
+print(y)
+
+Pythonclass = "Python class"
+print(Pythonclass)
+
+#make an input
+name = input("enter your name:")
+print("\n Hi", name , "\n welcome to the python class")
